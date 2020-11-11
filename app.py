5ed65230a422a283f26fe40150614d00638a0e57
Takaforyannick30 <takaforyannick30@gmail.com>
2020-11-11 12:10:48 +0000

    Add sign_up functionality to enable users to sign up

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -26,7 +27,27 @@ def home():
 
 
 @app.route("/sign_up", methods=["GET", "POST"])
+# def sign_up():
+#     return render_template("sign_up.html")
 def sign_up():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("sign_up"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("sign_up.html")
 
 
