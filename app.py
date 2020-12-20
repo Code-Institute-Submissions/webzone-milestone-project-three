850591974b4a4e04ed28f005204429ccc261d3e1
Takaforyannick30 <takaforyannick30@gmail.com>
2020-12-20 14:12:22 +0000

    Add error handler for '500 - internal server error'

--- app.py
+++ app.py
@@ -210,6 +210,12 @@ def page_not_found(error):
     return render_template("errors/404.html"), 404
 
 
+# Error handler for internal server error
+@app.errorhandler(500)
+def something_went_wrong(error):
+    return render_template("errors/500.html"), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
